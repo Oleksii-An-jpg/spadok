14b4ae96f284c0b9dfe15df5906d9a8e9f931393
oleksii.an <oleksii.a@gocheetah.com>
2025-11-27 19:36:44 +0200

    progress #29

--- src/app/(public)/collections/page.tsx
+++ src/app/(public)/collections/page.tsx
@@ -1,17 +1,17 @@
 'use server';
 
-import {Box, Heading, VStack} from "@chakra-ui/react";
+import {Grid, Heading, VStack} from "@chakra-ui/react";
 import {getCategories} from "@/api/categories";
 import Collection from "@/components/collection";
 
 export default async function Page() {
     const categories = await getCategories();
-    return <VStack align="stretch" gap={4}>
+    return <VStack align="stretch" gap={8}>
         <Heading size={{ base: '2xl', xl: '4xl' }} fontWeight="light">Наші колекції</Heading>
-        <Box columnCount={{ base: 3, md: 4, lg: 5, xl: 6 }} gap={4}>
+        <Grid templateColumns={{ base: 'repeat(2, 1fr)', xl: 'repeat(4, 1fr)' }} gap={4}>
             {categories.filter(category => category.isCollection).map(category => (
                 <Collection collection={category} key={category.id} />
             ))}
-        </Box>
+        </Grid>
     </VStack>
 }
