339df2e28ce753751fa30dcf1c016f4c58e301d9
oleksii.an <oleksii.a@gocheetah.com>
2025-12-14 16:53:51 +0200

    progress #48.1

--- src/app/(public)/(nocache)/page.tsx
+++ src/app/(public)/(nocache)/page.tsx
@@ -177,7 +177,7 @@ export default async function Home() {
                       <VStack>
                           <BrandButton asChild size="xl" variant="brand-primary">
                               <ChakraLink asChild>
-                                  <Link prefetch={false} href="/catalog">Перейти до каталогу</Link>
+                                  <Link prefetch={false} href="/catalog">Каталог</Link>
                               </ChakraLink>
                           </BrandButton>
                       </VStack>
